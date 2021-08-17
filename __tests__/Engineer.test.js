2ffeaf9250d59c10bcb2df11908877cb88dfbb27
Caroline <Cluce059@gmail.com>
2021-08-17 17:53:18 -0400

    engineer obj working

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,20 @@
 const { TestWatcher } = require('jest');
 const Engineer = require('../lib/Engineer');
 
-test('creates Engineer object', (name, id, email, github, officeNumber) => {
-    const Engineer = 
+test('creates Engineer object', () => {
+    const github = 'Davehub'
+    const engineer = new Engineer('Dave', 56, 'Dave@email.com',github);
+    expect(typeof(engineer)).toBe('object');
+});
+
+test('gets engineer github from getGithub', () =>{
+    const github ='Davehub';
+    const engineer = new Engineer('Dave', 56, 'Dave@email.com', github);
+    expect(engineer.getGithub()).toBe(github);
+});
+
+test('gets engineer role from getrole', () =>{
+    const role = 'Engineer'
+    const engineer = new Engineer('Dave', 56, 'Dave@email.com', 'Davehub');
+    expect(engineer.getRole()).toBe(role);
 });
