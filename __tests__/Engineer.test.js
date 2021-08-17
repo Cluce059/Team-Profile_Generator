350c029e1b2a7d4e8b0a66d7ba741228fb9f926c
Caroline <Cluce059@gmail.com>
2021-08-17 18:02:39 -0400

    engineer obj done

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,5 @@
 const { TestWatcher } = require('jest');
+const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 
 test('creates Engineer object', () => {
@@ -17,4 +18,21 @@ test('gets engineer role from getrole', () =>{
     const role = 'Engineer'
     const engineer = new Engineer('Dave', 56, 'Dave@email.com', 'Davehub');
     expect(engineer.getRole()).toBe(role);
+});
+
+test('gets engineer name from getName', () =>{
+    const name = 'Dave';
+    const employee = new Employee(name, 56, 'Dave@email.com', 'Davehub');
+    expect(employee.getName()).toBe(name);
+});
+
+test('gets engineer id from getId', () =>{
+    const engineer = new Engineer('Dave', 56, 'Dave@email.com', 'Davehub');
+    expect(engineer.getId()).toBe(56);
+});
+
+test('gets engineer email from getEmail', () =>{
+    const email = 'Dave@email.com';
+    const engineer = new Engineer('Dave', 56, email, 'Davehub');
+    expect(engineer.getEmail()).toBe(email);
 });
