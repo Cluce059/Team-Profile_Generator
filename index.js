3450932421bccb1a33405cf05be1a025370bc1c9
Caroline <Cluce059@gmail.com>
2021-08-18 15:03:26 -0400

    git user input

--- index.js
+++ index.js
@@ -5,26 +5,75 @@ const Intern = require('./lib/Intern');
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { inherits } = require('util');
 
-const team =[];
+const employees =[];
 
 function employeePrompt() {
     return inquirer.prompt([
         {
+            type: 'input',
+            name: 'name',
+            message: "What is the new team member's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID number?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email address?"
+        },
+        {
             type: 'list',
             message: 'What type of employee are you adding?',
-            type: 'type',
-            choices: ['Intern', 'Manager', 'Engineer', 'create profile']
-        },
-    ]).then(role => {
-        if(role.type === 'Intern'){
-            internPrompt();
-        } else if(role.type === 'Manager'){
-            managerPrompt();
-        } else if(role.type === 'Engineer'){
-            engineerPrompt();
-        } else if(role.name === 'create profile'){
-            renderProfile(output, render(team));
+            name: 'role',
+            choices: ['Intern', 'Manager', 'Engineer']
         }
+    ])
+    .then(function({name, id, email, role}){
+        let roleAttr = '';
+        if(role === 'Intern'){
+            roleAttr = 'school';
+            //console.log(roleAttr.value);
+        } else if(role === 'Manager'){
+            roleAttr = 'office number';
+        } else if(role === 'Engineer'){
+            roleAttr = 'github username';
+        }
+        //console.log(roleAttr);
+        inquirer.prompt([{
+            tye: 'input',
+            name: 'roleAttr',
+            message: `Enter new ${role}'s ${roleAttr}`
+        },
+        {
+            type: 'list',
+            name: 'addEmployee',
+            message: 'Would you like to add another team member?',
+            choices: ['Yes', 'No']
+        }    
+    ])
+    .then(function({roleAttr, addEmployee}){
+        let newEmployee;
+        if(role === 'Engineer'){
+            newEmployee = new Engineer(name, id, email, roleAttr);
+        } else if(role === 'Intern'){
+            newEmployee = new Intern(name, id, email, roleAttr);
+        } else if(role === 'Manager'){
+            newEmployee = new Manager(name, id, email, roleAttr);
+        }
+        employees.push(newEmployee);
+        console.log(newEmployee);
+    })
     });
 };
+
+
+function init(){
+    employeePrompt();
+};
+
+init();
